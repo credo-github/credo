7342fc500b8d0479535aab5c8ea0f6774322ac32
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-20 12:58:49 +0100

    chore: put naming in context above

--- templates/welcome/Docs (chosenLanguage === 'ruby')/hello.rb
+++
@@ -1,4 +0,0 @@
-class Welcome<%- capitalize(dashToCamel(name.split(' ').join('-'))) %>
-end
-
-welcomed_<%- name.split(' ').join('_').toLocaleLowerCase() %> = Welcome<%- capitalize(dashToCamel(name)) %>. new
