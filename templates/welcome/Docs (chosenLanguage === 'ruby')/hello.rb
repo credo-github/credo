2eebab729143cbd2e579c8c0abffe1a31a12dafb
Sam Hinton <samhinton@Sams-MacBook-Pro-2.local>
2022-04-20 12:55:12 +0100

    fix: name transform in ruby snippet

--- templates/welcome/Docs (chosenLanguage === 'ruby')/hello.rb
+++ templates/welcome/Docs (chosenLanguage === 'ruby')/hello.rb
@@ -1,4 +1,4 @@
-class Welcome<%- capitalize(dashToCamel(name)) %>
+class Welcome<%- capitalize(dashToCamel(name.split(' ').join('-'))) %>
 end
 
 welcomed_<%- name.split(' ').join('_').toLocaleLowerCase() %> = Welcome<%- capitalize(dashToCamel(name)) %>. new
